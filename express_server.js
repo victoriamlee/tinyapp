70be4d9b07825b62ff7bf80287612309679655c5
Victoria Lee <victorialee72@live.com>
2020-08-03 21:02:42 +0000

    Added body-parser

--- express_server.js
+++ express_server.js
@@ -9,6 +9,18 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+function generateRandomString() {
+
+}
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -16,6 +28,10 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
