71091061b0e35cab6bd80bf9b38df608a8bdb201
Victoria Lee <victorialee72@live.com>
2020-08-04 18:25:30 +0000

    Added delete button

--- express_server.js
+++ express_server.js
@@ -42,9 +42,15 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURL}`);         
 });
 
+
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
+});
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
 });
 
 // app.get("/urls.json", (req, res) => {
